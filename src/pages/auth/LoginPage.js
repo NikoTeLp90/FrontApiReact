8f4ae08c1780b54a68770b0ff9b0dc0a834ddea4
NikoTeLp90 <nikote.lp@gmail.com>
2025-06-11 12:09:43 -0300

    change endpoints

--- src/pages/auth/LoginPage.js
+++ src/pages/auth/LoginPage.js
@@ -15,7 +15,7 @@ function Login() {
     e.preventDefault();
 
     try {
-      const response = await fetch("http://127.0.0.1:5000/login", {
+      const response = await fetch("https://pruebaapi-yt30.onrender.com/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form),
