82ac160bccb9e9e2dd4c87511c9bd0568f320cf8
NikoTeLp90 <nikote.lp@gmail.com>
2025-06-11 11:52:25 -0300

    first commit

---
+++ src/pages/HomePage.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import '../css/Login.css'
+
+function Home() {
+ 
+  return (
+    <div className="Home">
+        <h1>Bienvenido al home</h1>
+    </div>
+  );
+}
+
+export default Home;
